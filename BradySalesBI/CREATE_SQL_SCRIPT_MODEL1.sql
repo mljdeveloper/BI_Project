21593313aa1bdee34dc0c0325bcf979b5ea9e5fc
mljdeveloper <mlj.developer.br@gmail.com>
2020-05-02 13:41:09 -0400

    Adicionado pacote de tempo

---
+++ BradySalesBI/CREATE_SQL_SCRIPT_MODEL1.sql
@@ -0,0 +1,28 @@
+﻿-- CREATING DE ETL SCRIPTS MODEL
+--***************************
+-- ETL PRE-LOAD TASKS
+--***************************
+-- 1) DROP FOREGIN KEYS
+-- 2) CLEAR FLUSH AND FILL TABLES
+
+TRUNCATE TABLE FACTSALES;
+TRUNCATE TABLE DIMENSIONS;
+
+
+--*****************************
+-- ETL DIMENSOIN LOAD TASKS
+--*****************************
+-- 3) LOAD FLUSH AND FILL DIMENSION TABLES
+-- 4) LOAD INCREMENTAL LOADING DIMENSION TABLES
+
+--******************************
+-- ETL FAC LOAD TASKS
+--******************************
+-- 5) LOAD FLUSH AND FILL DIMENSION TABLES
+-- 6) LOAD INCREMENTAL LOADING DIMENSION TABLES
+
+
+--**************************
+-- ETL POST-LOAD TASKS
+--**************************
+--7 REPLACE FOREGIN KEYS
